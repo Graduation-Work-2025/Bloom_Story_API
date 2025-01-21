d8932adfcf9fdb0b927f75c6c5ebcb9a86eb0e93
춘식 <142300831+Choon0414@users.noreply.github.com>
2025-01-21 09:36:14 +0900

    Initial commit

---
+++ src/main/java/java/bloom_story/BloomStoryApplication.java
@@ -0,0 +1,13 @@
+package java.bloom_story;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloomStoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloomStoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/java/bloom_story/BloomStoryApplicationTests.java
@@ -0,0 +1,13 @@
+package java.bloom_story;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloomStoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
