8ee27e9600df2ef028697130aafa6e5e482c63b6
Hwang HyeonSik <142300831+Choon0414@users.noreply.github.com>
2025-05-13 17:43:37 +0900

    fix: 잘못된 http 메서드 수정 (#23)
    * feat: 리포트 생성 API 작성
    
    * fix: 스토리 이미지 테이블 삭제
    
    * feat: 지피티 감정 레포트 기능 추가
    
    * fix: 이미지 url 추가
    
    * fix: HTTP 메서드 수정

--- src/main/java/bloom_story/domain/report/controller/ReportController.java
+++ src/main/java/bloom_story/domain/report/controller/ReportController.java
@@ -2,13 +2,13 @@ package bloom_story.domain.report.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import bloom_story.domain.report.dto.EmotionReportResponse;
-import bloom_story.domain.report.service.ReportService;
 import bloom_story.domain.report.dto.RecommendActivityResponse;
+import bloom_story.domain.report.service.ReportService;
 import bloom_story.global.domain.jwt.UserId;
 import lombok.RequiredArgsConstructor;
 
@@ -19,7 +19,7 @@ public class ReportController implements ReportApi {
 
     private final ReportService reportService;
 
-    @PostMapping("/emotions")
+    @GetMapping("/emotions")
     public ResponseEntity<EmotionReportResponse> getEmotionReport(
         @UserId Integer userId
     ) {
@@ -27,7 +27,7 @@ public class ReportController implements ReportApi {
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
-    @PostMapping("/recommend")
+    @GetMapping("/recommend")
     public ResponseEntity<RecommendActivityResponse> getRecommendActivity(
         @UserId Integer userId
     ) {
@@ -35,7 +35,7 @@ public class ReportController implements ReportApi {
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
-    @PostMapping("/keywords")
+    @GetMapping("/keywords")
     public ResponseEntity<EmotionReportResponse> getLastWeekKeyword(
         @UserId Integer userId
     ) {
